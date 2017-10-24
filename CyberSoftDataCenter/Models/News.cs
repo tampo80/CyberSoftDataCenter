2eebb459f4bad82cd7ad24626073504e13250aa2
3aba1 <aliamadouba@yahoo.fr>
2017-10-24 22:59:23 +0000

    Model News

---
+++ CyberSoftDataCenter/Models/News.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CyberSoftDataCenter.Models
+{
+    public class News
+    {
+        public int NewsId { get; set; }
+
+        [Required]
+        [Display(Name = "Titre")]
+        public string Title { get; set; }
+
+        public string Auteur { get; set; }
+
+        [Display(Name = "Date")]
+        public DateTime DateNews { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
